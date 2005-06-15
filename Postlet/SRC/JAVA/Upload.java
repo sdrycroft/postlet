26c8de288ac2741dd9bf9bb3d623b0acb9f22803
Simon Rycroft <sdrycroft@users.noreply.github.com>
2005-06-15 13:08:58 +0000

    Changed to now serial uploading (will probably change this back at some point).

--- Postlet/SRC/JAVA/Upload.java
+++ Postlet/SRC/JAVA/Upload.java
@@ -10,45 +10,47 @@
  */
 import java.net.*;
 
-public class Upload extends Thread
-{
+public class Upload extends Thread {
     String [] filenames;
     Main main;
     int [] fileSize;
     String destination;
     
     /** Creates a new instance of Upload */
-    public Upload(String [] f, int[] fs, Main m, String d)
-    {
+    public Upload(String [] f, int[] fs, Main m, String d) {
         filenames = f;
         main = m;
         fileSize = fs;
         destination = d;
     }
     
-    public void run()
-    {
-        for(int i=0; i<filenames.length; i+=10)
-        {    
+    public void run() {
+        /*
+        for(int i=0; i<filenames.length; i+=10) {
             int j=0;
-            while(j<10 && (i+j)<filenames.length)
-            {
-                UploadThread u[] = new UploadThread[filenames.length];                
-                try 
-                {
+            while(j<10 && (i+j)<filenames.length) {
+                UploadThread u[] = new UploadThread[filenames.length];
+                try {
                     InetAddress myAdr = InetAddress.getLocalHost();
                     String thisComp = myAdr.getHostAddress();
                     u[i+j] = new UploadThread(destination,filenames[i+j], fileSize[i+j], main);
                     u[i+j].start();
                     try{
-                        u[i+j].sleep(1000);}
-                    catch(InterruptedException ie){System.out.println("### exception");}
+                        u[i+j].sleep(1000);} catch(InterruptedException ie){System.out.println("### exception");}
                     j++;
-                }
-                catch (UnknownHostException e) {System.err.println(e.getMessage());}
-                
-                
+                } catch (UnknownHostException e) {System.err.println(e.getMessage());}
+         
+         
             }
+        }
+         */
+        for(int i=0; i<filenames.length; i++) {
+            UploadThread u = new UploadThread(destination,filenames[i], fileSize[i], main);
+            u.start();
+            // Wait for thread to finish
+            while(u.isAlive()){;}
+            System.out.println("HERE!");
+            System.gc();
         }
     }
 }
