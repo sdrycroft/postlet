cfe0862a4e7316f7dcb5707c1c18bc37abcd9d7e
sdrycroft <sdrycroft@3ce5dc16-4710-0410-8227-9078dca7a4d3>
2005-06-15 12:03:23 +0000

    *** empty log message ***

--- branches/garbagecollection/Postlet/SRC/JAVA/FileUploader.java
+++ branches/garbagecollection/Postlet/SRC/JAVA/FileUploader.java
@@ -2,42 +2,35 @@ import java.io.*;
 import java.net.*;
 import javax.swing.*;
 
-public class FileUploader 
-{
+public class FileUploader {
     HttpURLConnection httpURLConn;
     private static String boundary="leedsunitedarethebestteaminthepremierleagueHoHum1966andallthat";
     private static String twoHyphens="--";
     private static String lineEnd="\r\n";
-       
-    public FileUploader(String phpScript)
-    {
-       httpURLConn = null;
-       try
-       {
-          URL theURL = new URL(phpScript);
-          httpURLConn  = (HttpURLConnection)theURL.openConnection();          
-          httpURLConn.setRequestMethod("POST");
-          httpURLConn.setRequestProperty("Connection", "Keep-Alive");
-          httpURLConn.setDoOutput(true);
-          httpURLConn.setUseCaches(false);
-          httpURLConn.setRequestProperty("Accept-Charset", "iso-8859-1,*,utf-8");
-          httpURLConn.setRequestProperty("Accept-Language", "en");
-          httpURLConn.setRequestProperty("Content-type", "multipart/form-data; boundary=" + boundary);
-       }
-       catch (MalformedURLException ex){System.out.println("### exception");}
-       catch (IOException ioex){System.out.println("### exception");}
+    
+    public FileUploader(String phpScript) {
+        httpURLConn = null;
+        try {
+            URL theURL = new URL(phpScript);
+            httpURLConn  = (HttpURLConnection)theURL.openConnection();
+            httpURLConn.setRequestMethod("POST");
+            httpURLConn.setRequestProperty("Connection", "Keep-Alive");
+            httpURLConn.setDoOutput(true);
+            httpURLConn.setUseCaches(false);
+            httpURLConn.setRequestProperty("Accept-Charset", "iso-8859-1,*,utf-8");
+            httpURLConn.setRequestProperty("Accept-Language", "en");
+            httpURLConn.setRequestProperty("Content-type", "multipart/form-data; boundary=" + boundary);
+        } catch (MalformedURLException ex){System.out.println("### exception");} catch (IOException ioex){System.out.println("### exception");}
     }
     
-    public void uploadFile(String filename)
-    {
+    public void uploadFile(String filename) {
         DataOutputStream outStream;
         int bytesRead, bytesAvailable, bufferSize;
         byte[] buffer;
         int maxBufferSize = 1024*1024*1024;
         
-        try 
-        {
-            outStream = new DataOutputStream (httpURLConn.getOutputStream ());
+        try {
+            outStream = new DataOutputStream(httpURLConn.getOutputStream());
             outStream.writeBytes(twoHyphens + boundary + lineEnd);
             FileInputStream fileInputStream = new FileInputStream(new File(filename));
             outStream.writeBytes("Content-Disposition: form-data; name=\"userfile\";" + " filename=\""+ filename + "\"" + lineEnd);
@@ -49,44 +42,39 @@ public class FileUploader
             buffer = new byte[bufferSize];
             
             bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            while (bytesRead > 0) 
-            {
+            while (bytesRead > 0) {
                 System.gc();
-               outStream.write(buffer, 0, bufferSize);
-               bytesAvailable = fileInputStream.available();
-               bufferSize = Math.min(bytesAvailable,maxBufferSize);
-               
-               bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                outStream.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
             }
             fileInputStream.close();
             outStream.writeBytes(lineEnd);
-                        
+            
             outStream.writeBytes(lineEnd);
             
             outStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
             
-            outStream.flush ();
-            outStream.close ();
-        }
-        catch (IOException ioex){;}
+            outStream.flush();
+            outStream.close();
+        } catch (IOException ioex){;}
     }
     
-    public String getPOSTRequestResponse()
-    {
-       BufferedReader inStream;
-       StringBuffer results = new StringBuffer();
-       String newURL = "";
-       try
-       {
-          inStream = new BufferedReader(new InputStreamReader(httpURLConn.getInputStream()));
-          String str;
-          int i = 0;
-          while ((str = inStream.readLine()) != null)
-             results.append(str+"\n");
-          
-          inStream.close ();
-       }
-       catch (IOException ioex){;}
-       return results.toString();
+    public String getPOSTRequestResponse() {
+        BufferedReader inStream;
+        StringBuffer results = new StringBuffer();
+        String newURL = "";
+        try {
+            inStream = new BufferedReader(new InputStreamReader(httpURLConn.getInputStream()));
+            String str;
+            int i = 0;
+            while ((str = inStream.readLine()) != null)
+                results.append(str+"\n");
+            
+            inStream.close();
+        } catch (IOException ioex){;}
+        return results.toString();
     }
 }
