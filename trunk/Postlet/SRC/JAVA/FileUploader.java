f8fda63f7158ae6ccea22079596c1abf644d2c28
sdrycroft <sdrycroft@3ce5dc16-4710-0410-8227-9078dca7a4d3>
2005-08-05 12:51:33 +0000

    Basically the 1.2.2.1 branch (I'm not the best with CVS)

--- trunk/Postlet/SRC/JAVA/FileUploader.java
+++ trunk/Postlet/SRC/JAVA/FileUploader.java
@@ -1,88 +1,184 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.net.URL;
+import java.lang.Integer;
 import javax.swing.*;
 
-public class FileUploader 
-{
-    HttpURLConnection httpURLConn;
-    private static String boundary="leedsunitedarethebestteaminthepremierleagueHoHum1966andallthat";
-    private static String twoHyphens="--";
-    private static String lineEnd="\r\n";
-       
-    public FileUploader(String phpScript)
-    {
-       httpURLConn = null;
-       try
-       {
-          URL theURL = new URL(phpScript);
-          httpURLConn  = (HttpURLConnection)theURL.openConnection();          
-          httpURLConn.setRequestMethod("POST");
-          httpURLConn.setRequestProperty("Connection", "Keep-Alive");
-          httpURLConn.setDoOutput(true);
-          httpURLConn.setUseCaches(false);
-          httpURLConn.setRequestProperty("Accept-Charset", "iso-8859-1,*,utf-8");
-          httpURLConn.setRequestProperty("Accept-Language", "en");
-          httpURLConn.setRequestProperty("Content-type", "multipart/form-data; boundary=" + boundary);
-       }
-       catch (MalformedURLException ex){System.out.println("### exception");}
-       catch (IOException ioex){System.out.println("### exception");}
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class FileUploader {
+    private static String lotsHyphens="---------------------------";
+    private static String lineEnd="\n";
+    private String header, footer;
+    private File file;
+    private URL url;
+    private String boundary ="kjaslfkajsdhfnassfioausycntiasuhcn";
+    private Main main;
+    
+    public FileUploader(String phpScript, Main m) throws MalformedURLException{
+        url = new URL(phpScript);
+        main = m;
     }
     
-    public void uploadFile(String filename)
-    {
-        DataOutputStream outStream;
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1024;
+    // Throw the host exception as will be handled in the same way as the
+    // malformed URL exception (this will stop all threads, as will affect
+    // all files).
+    public void uploadFile(String filename) {
         
-        try 
-        {
-            outStream = new DataOutputStream (httpURLConn.getOutputStream ());
-            outStream.writeBytes(twoHyphens + boundary + lineEnd);
-            FileInputStream fileInputStream = new FileInputStream(new File(filename));
-            outStream.writeBytes("Content-Disposition: form-data; name=\"userfile\";" + " filename=\""+ filename + "\"" + lineEnd);
-            outStream.writeBytes(lineEnd);
+        try {
+            file = new File(filename);
+            this.setBoundary();
+            this.setHeaderAndFooter();
             
-            bytesAvailable = fileInputStream.available();
+            Socket sock = new Socket(url.getHost(),80);
+            DataOutputStream output = new DataOutputStream(new BufferedOutputStream(sock.getOutputStream()));
+            BufferedReader input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             
-            int byteRead=1;
-            while(byteRead != -1)
-            {       
-                byteRead = fileInputStream.read();
-                outStream.write(byteRead);  
-                outStream.flush();              
+            output.writeBytes(header);
+            
+            byte [] byteBuff = new byte[1024];
+            FileInputStream fileStream = new FileInputStream(file);
+            int numBytes = 0;
+            
+            if (file.length()>Integer.MAX_VALUE){
+                throw new IOException("File is too large for upload");
             }
             
-            outStream.flush();
-            fileInputStream.close();
-            outStream.writeBytes(lineEnd);
-                        
-            outStream.writeBytes(lineEnd);
+            /////////////////////////////////////////////////////////////     
+            int maxBufferSize = 1024;
+            int bytesAvailable = fileStream.available();
+            System.out.println("File available: "+bytesAvailable);
+            int bufferSize = Math.min(bytesAvailable,maxBufferSize);
             
-            outStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+            byte buffer [] = new byte[bufferSize];
             
-            outStream.flush ();
-            outStream.close ();
+            int bytesRead = fileStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0) 
+            {
+               output.write(buffer, 0, bufferSize);
+               bytesAvailable = fileStream.available();
+               bufferSize = Math.min(bytesAvailable,maxBufferSize);
+               
+               bytesRead = fileStream.read(buffer, 0,  bufferSize);
+            }
+            ///////////////////////////////////////////////////////////
+            
+            output.writeBytes(footer);
+            
+            // Seems to be a bug in the way that java (or Apache), or more likely
+            // I handle the file sizes and thus the content-length (which is always
+            // too high).  Thus, send blanks till the apache server finishes
+            // listening for input.
+            
+            // Shouldn't need more than 1KB
+            for (int i = 0; i<1024; i++){
+                output.write(0);
+            }
+            
+            output.flush();
+            //output.close();
+            
+            StringBuffer replyString = new StringBuffer();
+            
+            System.out.println("Starting reading of output");
+            String line = "";
+            while ((line = input.readLine())!=null){
+                System.out.println("Reading output");
+                replyString.append(line+lineEnd);
+            }
+            System.out.println("Finished reading output");
+            //output.close();
+            input.close();
+            output.close();
+            sock.close();
+            
+            main.setProgress((int)file.length());
+            
+            output.close();
+            
+            header = null;
+            footer = null;
+            boundary = null;
+            file = null;
+            url = null;
+            
+        } catch(IOException ioe){
+            // Some kind of error caught
+            System.out.println(ioe.getMessage());
+            ioe.printStackTrace(System.out);
         }
-        catch (IOException ioex){;}
     }
     
-    public String getPOSTRequestResponse()
-    {
-       BufferedReader inStream;
-       StringBuffer results = new StringBuffer();
-       String newURL = "";
-       try
-       {
-          inStream = new BufferedReader(new InputStreamReader(httpURLConn.getInputStream()));
-          String str;
-          int i = 0;
-          while ((str = inStream.readLine()) != null)
-             results.append(str+"\n");
-          
-          inStream.close ();
-       }
-       catch (IOException ioex){;}
-       return results.toString();
+    public String getPostRequestResponse(){
+        
+        return "";
+    }
+    
+    private void setBoundary(){
+        
+        
+    }
+    
+    private void setHeaderAndFooter(){
+        
+        header = new String();
+        footer = new String();
+        
+        String afterContentLength = lotsHyphens +"--"+ boundary + lineEnd +
+                                    "Content-Disposition: form-data; name=\"userfile\"; filename=\""+file.getName()+"\""+lineEnd+
+                                    "Content-Type: application/octet-stream"+lineEnd+lineEnd;
+        
+        footer = lineEnd + lineEnd + "--"+ lotsHyphens+boundary+"--"+lineEnd;
+        
+        header="POST ";
+        header +=url.getFile();
+        header +=" HTTP/1.1";
+        header +=lineEnd;
+               
+        header +="Host: ";
+        header +=url.getHost();
+        header +=lineEnd;
+        
+        header +="User-Agent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.10)";
+        header +=lineEnd;
+        
+        header +="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+        header += lineEnd;
+        
+        header +="Accept-Language: en-us,en;q=0.5";
+        header += lineEnd;
+        
+        header +="Accept-Encoding: gzip,deflate";
+        header +=lineEnd;
+        
+        header +="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+        header += lineEnd;
+        
+        header +="Keep-Alive: 300";
+        header += lineEnd;
+        
+        header +="Connection: keep-alive";
+        header += lineEnd;
+        
+        header +="Content-Type: multipart/form-data; boundary=";
+        header +=lotsHyphens;
+        header +=boundary;
+        header +=lineEnd;
+                       
+        header +="Content-Length: ";
+        header += ""+(file.length()+header.length()+afterContentLength.length()+footer.length());
+        header +=lineEnd;
+        header +=lineEnd;
+                
+        header +=afterContentLength;
     }
 }
