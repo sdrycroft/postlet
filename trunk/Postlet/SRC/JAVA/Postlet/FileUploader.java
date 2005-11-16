ffd39cba8daa533b24f12c0cbe509950f8744491
sdrycroft <sdrycroft@3ce5dc16-4710-0410-8227-9078dca7a4d3>
2005-11-16 13:39:53 +0000

    Changed package to Postlet

---
+++ trunk/Postlet/SRC/JAVA/Postlet/FileUploader.java
@@ -0,0 +1,187 @@
+package Postlet;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.Socket;
+import java.net.URL;
+import java.lang.Integer;
+import javax.swing.*;
+
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class FileUploader {
+    private static String lotsHyphens="---------------------------";
+    private static String lineEnd="\n";
+    private String header, footer, request;
+    private File file;
+    private URL url;
+    private String boundary ="kjaslfkajsdhfnassfioausycntiasuhcn";
+    private Main main;
+    
+    public FileUploader(String phpScript, Main m) throws MalformedURLException{
+        url = new URL(phpScript);
+        main = m;
+    }
+    
+    // Throw the host exception as will be handled in the same way as the
+    // malformed URL exception (this will stop all threads, as will affect
+    // all files).
+    public void uploadFile(String filename) {
+        
+        try {
+            file = new File(filename);
+            this.setBoundary();
+            this.setHeaderAndFooter();
+            
+            Socket sock = new Socket(url.getHost(),80);
+            DataOutputStream output = new DataOutputStream(new BufferedOutputStream(sock.getOutputStream()));
+            BufferedReader input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            
+            output.writeBytes(request);
+            output.writeBytes(header);
+            
+            byte [] byteBuff = new byte[1024];
+            FileInputStream fileStream = new FileInputStream(file);
+            int numBytes = 0;
+            
+            if (file.length()>Integer.MAX_VALUE){
+                throw new IOException("File is too large for upload");
+            }
+            
+            // Following reads the file, and streams it.
+            /////////////////////////////////////////////////////////////     
+            int maxBufferSize = 1024;
+            int bytesAvailable = fileStream.available();
+            System.out.println("File available: "+bytesAvailable);
+            int bufferSize = Math.min(bytesAvailable,maxBufferSize);
+            
+            byte buffer [] = new byte[bufferSize];
+            
+            int bytesRead = fileStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0) 
+            {
+               output.write(buffer, 0, bufferSize);
+               bytesAvailable = fileStream.available();
+               bufferSize = Math.min(bytesAvailable,maxBufferSize);
+               
+               bytesRead = fileStream.read(buffer, 0,  bufferSize);
+            }
+            ///////////////////////////////////////////////////////////
+            
+            output.writeBytes(footer);
+            
+            // Seems to be a bug in the way that java (or Apache), or more likely
+            // I handle the file sizes and thus the content-length (which is always
+            // too high).  Thus, send blanks till the apache server finishes
+            // listening for input.            
+            // Shouldn't need more than 1KB
+            /*
+             for (int i = 0; i<1024; i++){
+                output.write(0);
+            }
+             */
+            
+            output.flush();
+            //output.close();
+            
+            StringBuffer replyString = new StringBuffer();
+            
+            String line = "";
+            while ((line = input.readLine())!=null){
+                replyString.append(line+lineEnd);
+            }
+            //output.close();
+            input.close();
+            output.close();
+            sock.close();
+            
+            main.setProgress((int)file.length());
+            
+            output.close();
+            
+            header = null;
+            footer = null;
+            boundary = null;
+            file = null;
+            url = null;
+            
+        } catch(IOException ioe){
+            // Some kind of error caught
+            System.out.println(ioe.getMessage());
+            ioe.printStackTrace(System.out);
+        }
+    }
+    
+    public String getPostRequestResponse(){
+        
+        return "";
+    }
+    
+    private void setBoundary(){
+        
+        
+    }
+    
+    private void setHeaderAndFooter(){
+        
+        header = new String();
+        footer = new String();
+        request = new String();
+        
+        String afterContent = lotsHyphens +"--"+ boundary + lineEnd +
+                                    "Content-Disposition: form-data; name=\"userfile\"; filename=\""+file.getName()+"\""+lineEnd+
+                                    "Content-Type: application/octet-stream"+lineEnd+lineEnd;
+        
+        footer = lineEnd + lineEnd + "--"+ lotsHyphens+boundary+"--"+lineEnd;
+        
+        request="POST ";
+        request +=url.getFile();
+        request +=" HTTP/1.1";
+        request +=lineEnd;
+               
+        header +="Host: ";
+        header +=url.getHost();
+        header +=lineEnd;
+        
+        header +="User-Agent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.10)";
+        header +=lineEnd;
+        
+        header +="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
+        header += lineEnd;
+        
+        header +="Accept-Language: en-us,en;q=0.5";
+        header += lineEnd;
+                
+        header +="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
+        header += lineEnd;
+                
+        header +="Content-Type: multipart/form-data; boundary=";
+        header +=lotsHyphens;
+        header +=boundary;
+        header +=lineEnd;
+                       
+        header +="Content-Length: ";
+        header += ""+(file.length()+afterContent.length());
+        //header += ""+file.length();
+        header +=lineEnd;
+        header +=lineEnd;
+                
+        header +=afterContent;
+        
+        System.out.println("****************************************************************");
+        System.out.println("HeaderLength: "+header.length());
+        System.out.println("FooterLength: "+footer.length());
+        System.out.println("RequestLength: "+request.length());
+        System.out.println("FileLength: "+file.length());
+        System.out.println("Hypens Length: "+lotsHyphens.length());
+        System.out.println("Boundary Length: "+boundary.length());
+        System.out.println("AfterContent length: "+afterContent.length());
+    }
+}
---
+++ trunk/Postlet/SRC/JAVA/Postlet/Main.java
@@ -0,0 +1,357 @@
+package Postlet;
+import java.awt.*;
+import java.util.*;
+import java.applet.Applet;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.io.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import java.net.*;
+import javax.swing.JDialog;
+import java.lang.*;
+import javax.swing.JApplet;
+
+
+public class Main extends JApplet implements MouseListener {
+    JScrollPane scrollPane;
+    JTable table;
+    JPanel rightPanel;
+    JButton add, remove, upload, help;
+    String rowData [][];
+    String filenames[];
+    TableColumn left, middle, right;
+    TableData tabledata;
+    FileUploader fu;
+    File [] file;
+    JLabel progressLabel;
+    JProgressBar progBar;
+    int sentBytes;
+    int fileSize[];
+    int totalBytes;
+    Font font;
+    String destination;
+    URL endpage, helppage;
+    Color backgroundColour, columnHeadColourBack, columnHeadColourFore;
+    
+    public void init() {
+        try {
+            // Set the look of the applet to be the same as the system standard
+            // of the computer that the applet is running on.
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (UnsupportedLookAndFeelException exc){;} catch (IllegalAccessException exc){;} catch (ClassNotFoundException exc){;} catch (InstantiationException exc){;}
+            
+            // Get the destination which is set by a parameter.
+            try {
+                URL dest = new URL(getParameter("destination"));
+                destination = getParameter("destination");
+                System.out.println("Destination has been set to:"+destination);
+            } catch(java.net.MalformedURLException malurlex){
+                // Do something here for badly formed destination, which is ESENTIAL.
+                JOptionPane message = new JOptionPane();
+                JOptionPane.showMessageDialog(null, "The destination URL provided is not a valid one.","Postlet error.", JOptionPane.ERROR_MESSAGE);
+                destination = null;
+            } catch(java.lang.NullPointerException npe){
+                // Do something here for the missing destination, which is ESENTIAL.
+                JOptionPane message = new JOptionPane();
+                JOptionPane.showMessageDialog(null, "You have not provided a destination URL.", "Postlet error.", JOptionPane.ERROR_MESSAGE);
+                destination = "http://darwin.zoology.gla.ac.uk/~sdrycroft/javaUpload.php";
+            }
+            
+            try {
+                // Set the background color, which is set by a parameter.
+                Integer redInteger = new Integer(getParameter("red"));
+                int red = redInteger.intValue();
+                Integer greenInteger = new Integer(getParameter("green"));
+                int green = greenInteger.intValue();
+                Integer blueInteger = new Integer(getParameter("blue"));
+                int blue = blueInteger.intValue();
+                backgroundColour = new Color(red, green, blue);
+                System.out.println("Background colour has been set to:"+red+"/"+green+"/"+blue);
+                // Try and dispose of all variables when finished with (memory is essential).
+                redInteger= null;
+                blueInteger= null;
+                greenInteger = null;
+                
+                // Set the background colour of the table headers.
+                redInteger = new Integer(getParameter("redheaderback"));
+                int redheaderback = redInteger.intValue();
+                greenInteger = new Integer(getParameter("greenheaderback"));
+                int greenheaderback = greenInteger.intValue();
+                blueInteger = new Integer(getParameter("blueheaderback"));
+                int blueheaderback = blueInteger.intValue();
+                columnHeadColourBack = new Color(redheaderback, greenheaderback, blueheaderback);
+                redInteger = null;
+                blueInteger= null;
+                greenInteger = null;
+                
+                // Set the foreground colour of the table headers.
+                redInteger = new Integer(getParameter("redheader"));
+                int redheader = redInteger.intValue();
+                greenInteger = new Integer(getParameter("greenheader"));
+                int greenheader = greenInteger.intValue();
+                blueInteger = new Integer(getParameter("blueheader"));
+                int blueheader = blueInteger.intValue();
+                columnHeadColourFore = new Color(redheader, greenheader, blueheader);
+                // Try and dispose of all variables when finished with (memory is essential).
+                redInteger = null;
+                blueInteger= null;
+                greenInteger = null;
+            } catch(java.lang.NullPointerException npered){
+                // Color isn't set.
+                // Just ignore this, and set the background color to the
+                // default one.
+                backgroundColour = null;
+                columnHeadColourFore = null;
+                columnHeadColourBack = null;
+            } catch(java.lang.NumberFormatException numfe){
+                // Color isn't set.
+                // Just ignore this, and set the background color to the
+                // default one.
+                backgroundColour = null;
+                columnHeadColourFore = null;
+                columnHeadColourBack = null;
+            }
+            
+            // Get the main pane to add content to.
+            Container pane = getContentPane();
+            
+            // Table for the adding of Filenames and sizes to.
+            tabledata = new TableData();
+            table = new JTable(tabledata);
+            table.setColumnSelectionAllowed(false);
+            //table.setDragEnabled(false);
+            table.getColumn("Filename").setMinWidth(300);
+            if (columnHeadColourBack != null && backgroundColour != null){
+                table.getTableHeader().setBackground(columnHeadColourBack);
+                table.getTableHeader().setForeground(columnHeadColourFore);
+                table.setBackground(backgroundColour);
+                //table.getTableHeader().setForeground(columnHeadColour);
+            }
+            scrollPane = new JScrollPane(table);
+            scrollPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+            
+            // Add the scroll pane/table to the main pane
+            pane.add(scrollPane, BorderLayout.CENTER);
+            
+            rightPanel = new JPanel(new GridLayout(4,1,10,10));
+            rightPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+            
+            add = new JButton("Add");
+            add.addMouseListener(this);
+            rightPanel.add(add);
+            
+            remove = new JButton("Remove");
+            remove.addMouseListener(this);
+            remove.setEnabled(false);
+            rightPanel.add(remove);
+            
+            upload = new JButton("Upload");
+            upload.addMouseListener(this);
+            upload.setEnabled(false);
+            rightPanel.add(upload);
+            
+            help = new JButton("Help");
+            help.addMouseListener(this);
+            rightPanel.add(help);
+            pane.add(rightPanel,"East");
+            
+            JPanel progPanel = new JPanel(new GridLayout(1, 3));
+            progPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+            
+            JLabel progCompletion = new JLabel("Upload progress: ",SwingConstants.RIGHT);
+            progPanel.add(progCompletion);
+            
+            progBar = new JProgressBar();
+            progPanel.add(progBar);
+            
+            progressLabel = new JLabel("",SwingConstants.CENTER);
+            progressLabel.setFont(font);
+            //progressLabel.setForeground(blue);
+            progPanel.add(progressLabel);
+            
+            
+            if (backgroundColour != null){
+                pane.setBackground(backgroundColour);
+                rightPanel.setBackground(backgroundColour);
+                scrollPane.setBackground(backgroundColour);
+                progPanel.setBackground(backgroundColour);
+                // Always set the table background colour as White.
+                // May change this if required, only would require alot of Params!
+                scrollPane.getViewport().setBackground(Color.white);
+            }
+            
+            pane.add(progPanel,"South");
+            
+            // If the destination has not been set/isn't a proper URL
+            // Then deactivate the buttons.
+            if (destination == null) {
+                remove.setEnabled(false);
+                add.setEnabled(false);
+                upload.setEnabled(false);
+            }
+        } catch (java.lang.OutOfMemoryError oomerr){
+            System.out.println("OUT OF MEMORY HERE!");
+        }
+    }
+    
+    public void removeClick() {
+        if(table.getSelectedRowCount()>0) {
+            File [] fileTemp = new File[file.length-table.getSelectedRowCount()];
+            int k=0;
+            for(int i=0; i<file.length-table.getSelectedRowCount(); i++) {
+                for(int j=0; j<table.getSelectedRowCount(); j++) {
+                    if(i==table.getSelectedRows()[j]) {
+                        k++;
+                    }
+                    fileTemp[i] = file[i+k];
+                }
+            }
+            file = fileTemp;
+            tableUpdate();
+        }
+        if (file.length==0) {
+            upload.setEnabled(false);
+            remove.setEnabled(false);
+        }
+    }
+    
+    public void uploadClick() {
+        if(filenames !=null) {
+            JOptionPane message = new JOptionPane();
+            JOptionPane.showMessageDialog(null, "Do not close your web browser, or leave this page until upload completes.", "Postlet warning.", JOptionPane.INFORMATION_MESSAGE);
+            add.setEnabled(false);
+            remove.setEnabled(false);
+            help.setEnabled(false);
+            upload.setEnabled(false);
+            sentBytes = 0;
+            progBar.setMaximum(totalBytes);
+            progBar.setMinimum(0);
+            Upload u = new Upload(filenames, fileSize, this, destination);
+            u.start();
+        }
+    }
+    
+    public void setProgress(int a) {
+        sentBytes += a;
+        progBar.setValue(sentBytes);
+        if (sentBytes == totalBytes){
+            progressLabel.setText("FINISHED");
+            try {
+                endpage = new URL(getParameter("endpage"));
+                getAppletContext().showDocument(endpage);
+                
+            } catch(java.net.MalformedURLException malurlex){
+                // Just ignore this error, as it is most likely from the endpage
+                // not being set.
+                System.err.println("Endpage unset or not valid URL ("+getParameter("endpage")+")");
+            }
+        }
+    }
+    
+    public void tableUpdate() {
+        totalBytes = 0;
+        filenames = new String[file.length];
+        fileSize = new int[file.length];
+        for(int i=0; i<file.length; i++) {
+            filenames[i] = file[i].getAbsolutePath();
+            fileSize[i] = (int)file[i].length();
+            totalBytes += (int)file[i].length();
+        }
+        int i=0;
+        rowData = new String[255][2];
+        while(i<file.length) {
+            rowData[i][0] = file[i].getName();
+            rowData[i][1] = ""+file[i].length();
+            i++;
+        }
+        tabledata.formatTable(rowData,i);
+        table.getColumn("Filename").setMinWidth(300);
+        repaint();
+    }
+    
+    public void addClick() {
+        JFileChooser chooser = new JFileChooser();
+        
+        progressLabel.setText("");
+        progBar.setValue(0);
+        UploaderFileFilter filter = new UploaderFileFilter();
+        filter.addExtension("jpg");
+        filter.addExtension("jpeg");
+        filter.addExtension("gif");
+        filter.addExtension("bmp");
+        filter.addExtension("png");
+        filter.addExtension("raw");
+        filter.addExtension("tif");
+        filter.setDescription("Image files");
+        chooser.setFileFilter(filter);
+        
+        
+        chooser.setAccessory(new FilePreviewer(chooser));
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        chooser.setMultiSelectionEnabled(true);
+        chooser.getSelectedFile();
+        chooser.setDialogTitle("Select file for upload");
+        int returnVal = chooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            file = chooser.getSelectedFiles();
+            tableUpdate();
+        }
+        if (file.length>0) {
+            upload.setEnabled(true);
+            remove.setEnabled(true);
+        }
+    }
+    
+    public void helpClick() {
+        // Open a web page in another frame/window
+        // Unless specified as a parameter, this will be a help page
+        // on the postlet website.
+        
+        // Get the help page.
+        String helpUrl;
+        helpUrl = getParameter("helppage");
+        if (helpUrl == null){
+            helpUrl = "http://postlet.sourceforge.net/help/";
+        }
+        System.out.println("Help page is:"+helpUrl);
+        try {
+            getAppletContext().showDocument(new URL(helpUrl), "_blank");
+        } catch (MalformedURLException helpexception){
+            // Show a popup with help instead!
+            System.err.println("Error with help dialog");
+            JOptionPane message = new JOptionPane();
+            JOptionPane.showMessageDialog(null, "The help URL provided is not a valid one.","Postlet error.", JOptionPane.ERROR_MESSAGE);
+            message.setVisible(true);
+        }
+        
+    }
+    
+// Here for testing purposes
+    public static void main(String args[]) {
+        Frame f = new Frame("Uploader");
+        Main main = new Main();
+        
+        main.init();
+        
+        f.add("Center", main);
+        f.pack();
+        f.setSize(600,200);
+        f.setVisible(true);
+    }
+    
+    
+    public void mouseClicked(MouseEvent e) {
+        if(e.getSource()==add && add.isEnabled())           {addClick();}
+        if(e.getSource()==upload && upload.isEnabled())     {uploadClick();}
+        if(e.getSource()==remove && remove.isEnabled())     {removeClick();}
+        if(e.getSource()==help && help.isEnabled())         {helpClick();}
+    }
+    
+    public void mouseEntered(MouseEvent e){;}
+    public void mouseExited(MouseEvent e){;}
+    public void mousePressed(MouseEvent e){;}
+    public void mouseReleased(MouseEvent e){;}
+    
+}
