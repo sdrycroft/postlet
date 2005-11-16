7d512ccc3cc9339e07f7e52f483674beb5981817
sdrycroft <sdrycroft@3ce5dc16-4710-0410-8227-9078dca7a4d3>
2005-11-16 13:50:20 +0000

    Removed FilePreviewer

--- trunk/Postlet/SRC/JAVA/Main.java
+++ trunk/Postlet/SRC/JAVA/Main.java
@@ -1,3 +1,19 @@
+/*  Copyright (C) 2005 Simon David Rycroft
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
+
 import java.awt.*;
 import java.util.*;
 import java.applet.Applet;
@@ -285,9 +301,7 @@ public class Main extends JApplet implements MouseListener {
         filter.addExtension("tif");
         filter.setDescription("Image files");
         chooser.setFileFilter(filter);
-        
-        
-        chooser.setAccessory(new FilePreviewer(chooser));
+                
         chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
         chooser.setMultiSelectionEnabled(true);
         chooser.getSelectedFile();
