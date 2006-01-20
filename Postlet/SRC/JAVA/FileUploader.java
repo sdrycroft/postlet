3ebef7dcfa245f651cb64826f3ea0e5054863caa
Simon Rycroft <sdrycroft@users.noreply.github.com>
2006-01-20 12:34:40 +0000

    0.6 commit

--- Postlet/SRC/JAVA/FileUploader.java
+++ Postlet/SRC/JAVA/FileUploader.java
@@ -87,79 +87,78 @@ public class FileUploader {
             }
             catch (NullPointerException npe){
                 sock = new Socket(url.getHost(),80);}
-            
+
             // Output stream, for writing to the socket.
             DataOutputStream output = new DataOutputStream(new BufferedOutputStream(sock.getOutputStream()));
             // Reader for accepting the reply from the server.
             BufferedReader input = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            
+
             // Write the request, and the header.
             output.writeBytes(request);
             output.writeBytes(header);
 
-            
             // Following reads the file, and streams it.
             /////////////////////////////////////////////////////////////             
             FileInputStream fileStream = new FileInputStream(file);
             int numBytes = 0;
-            
+
             if (file.length()>Integer.MAX_VALUE){
                 throw new IOException("File is too large for upload");
             }
-            
+
             // Size of buffer - May need reducing if users encounter
             // memory issues.
             int maxBufferSize = 1024*256;
             int bytesAvailable = fileStream.available();
             System.out.println("File available: "+bytesAvailable);
             int bufferSize = Math.min(bytesAvailable,maxBufferSize);
-            
+
             byte buffer [] = new byte[bufferSize];
-            
+
             int bytesRead = fileStream.read(buffer, 0, bufferSize);
             while (bytesRead > 0) 
             {
                output.write(buffer, 0, bufferSize);
                bytesAvailable = fileStream.available();
                bufferSize = Math.min(bytesAvailable,maxBufferSize);
-               
+
                bytesRead = fileStream.read(buffer, 0,  bufferSize);
             }
             ///////////////////////////////////////////////////////////
-            
+
             output.writeBytes(footer);
-                        
+
             output.flush();
-                        
+
             // Read the reply
-            String line;      
+            String line;
             reply ="";
             while ((line = input.readLine())!=null){
                 reply += line;
             }
-            
+
             // Close the socket and streams.
             input.close();
             output.close();
             sock.close();
-            
+
             // Set the progress, that this file has uploaded.
             main.setProgress((int)file.length());
-                        
+
         } catch(IOException ioe){
             // Some kind of error caught
             System.out.println(ioe.getMessage());
             ioe.printStackTrace(System.out);
         }
     }
-    
+
     public String getPostRequestResponse(){
-        
+
         return reply;
     }
-    
+
     private void setBoundary(int length){
-        
+
         char [] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         Random r = new Random();
         String boundaryString ="";
@@ -167,22 +166,22 @@ public class FileUploader {
             boundaryString += alphabet[r.nextInt(alphabet.length)];
         boundary = boundaryString;
     }
-    
+
     private void setHeaderAndFooter(){
-        
+
         header = new String();
         footer = new String();
         request = new String();
-        
+
         // AfterContent is what is sent after the Content-Length header field,
         // but before the file itself.  The length of this, is what is required
         // by the content-length header (along with the length of the file).
         String afterContent = lotsHyphens +"--"+ boundary + lineEnd +
                                     "Content-Disposition: form-data; name=\"userfile\"; filename=\""+file.getName()+"\""+lineEnd+
                                     "Content-Type: application/octet-stream"+lineEnd+lineEnd;
-        
+
         footer = lineEnd + lineEnd + "--"+ lotsHyphens+boundary+"--"+lineEnd;
-        
+
         // The request includes the absolute URI to the script which will
         // accept the file upload.  This is perfectly valid, although it is
         // normally only used by a client when connecting to a proxy server.
@@ -191,18 +190,18 @@ public class FileUploader {
         request +=url.toExternalForm();
         request +=" HTTP/1.1";
         request +=lineEnd;
-               
+
         // Host that we are sending to (not necesarily connecting to, if behind
         // a proxy)
         header +="Host: ";
         header +=url.getHost();
         header +=lineEnd;
-        
+
         // Give a  user agent just for completeness.  This could be changed so that
         // access by the Postlet applet can be logged.
         header +="User-Agent: Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.7.10)";
         header +=lineEnd;
-        
+
         // Standard accept
         header +="Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5";
         header += lineEnd;        
@@ -210,21 +209,21 @@ public class FileUploader {
         header += lineEnd;                
         header +="Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
         header += lineEnd;
-                
+
         // What we are sending.
         header +="Content-Type: multipart/form-data; boundary=";
         header +=lotsHyphens;
         header +=boundary;
         header +=lineEnd;
-                       
+
         // Length of what we are sending.
         header +="Content-Length: ";
         header += ""+(file.length()+afterContent.length()+footer.length());
         header +=lineEnd;
         header +=lineEnd;
-                
+
         header +=afterContent;
-        
+
         // Debugging.  Left in as prints to Java console, and could prove useful
         // for people using the applet.
         System.out.println("****************************************************************");
--- Postlet/SRC/JAVA/PostletLabels.java
+++ Postlet/SRC/JAVA/PostletLabels.java
@@ -23,7 +23,15 @@ import java.io.InputStreamReader;
 
 public class PostletLabels {
     
-    // languages listed by their English/British names.
+    // languages listed by their two letter codes (http://www.w3.org/WAI/ER/IG/ert/iso639.htm)
+    // The compiled in languages will be:
+	// English
+	// Spainish
+	// Dutch
+	// German
+	// French
+	// Italian
+	// Portuguese
     public static final String languages [] = { "EN" , "DE" , "NL"};    
     private String [] language;     
     private static final int numLabels = 16;
@@ -48,21 +56,21 @@ public class PostletLabels {
                 
     private static final String [] DE = {
                 "Dateiname",
-                "Dateigröße",
+                "DateigrÃ¶Ãe",
                 "Fertig",
-                "Die angegebene Ziel-URL ist ungültig.",
+                "Die angegebene Ziel-URL ist ungÃ¼ltig.",
                 "Es ist keine Ziel-URL angegeben.",
                 "Postlet Fehler",
-                "Hinzufügen",
+                "HinzufÃ¼gen",
                 "Entfernen",
                 "Upload",
                 "Hilfe",       
                 "Upload prozess:",
-                "Der Browser darf nicht geschlossen werden solange der Upload läuft.",
+                "Der Browser darf nicht geschlossen werden solange der Upload lÃ¤uft.",
                 "Postlet Warnung",
                 "Bild-Dateien",
-                "Datei zum hochladen auswählen",
-                "Die angegebene Hilfe-URL ist nicht gültig."};
+                "Datei zum hochladen auswÃ¤hlen",
+                "Die angegebene Hilfe-URL ist nicht gÃ¼ltig."};
                 
     private static final String [] NL = {
                 "Bestands naam",
@@ -81,7 +89,61 @@ public class PostletLabels {
                 "Plaatjes bestanden",
                 "Selecteer bestand voor upload",
                 "De help URL is niet correct"};
-                
+
+    private static final String [] FR = {
+		"Nom de fichier", //0 
+		"Taille du fichier", //1 
+		"TerminÃ©", //2 
+		"L'URL de destination fournie n'est pas valide.", //3 
+		"Vous n'avez pas fourni d'URL de destination.", //4 
+		"Erreur Postlet", //5 
+		"Ajouter", //6 
+		"Enlever", //7 
+		"Envoyer", //8 
+		"Aide", //9 
+		"Progression de l'envoi:", //10 
+		"Ne fermer pas votre browser et rester sur cette page jusqu'Ã  l'envoi complet des donnÃ©es.", //11 
+		"Avertissement Postlet", //12 
+		"Fichiers image", //13 
+		"Selectionner les fichiers Ã  envoyer", //14 
+		"L'URL de l'aide fournie n'est pas valide."}; //15 
+
+    private static final String [] ES = {
+                "Fichero",             //0
+                "TamaÃ±o",            //1
+                "Terminado",             //2
+                "La URL destino no es vÃ¡lida.", //3
+                "No hay una URL destino especificada.",         //4
+                "Postlet error",        //5
+                "AÃ±adir",                  //6
+                "Quitar",               //7
+                "Subir",               //8
+                "Ayuda",                 //9
+                "Progreso de la transferencia:",     //10
+                "No cerrar el navegador o esta ventana hasta que la transferencia haya terminado.", //11
+                "Aviso del Postlet",      //12
+                "Ficheros de imÃ¡genes",          //13
+                "Selecciona los ficheros para subir", //14
+                "La URL de ayuda no es vÃ¡lida."}; //15
+
+    private static final String [] IT = {
+		"Nome File",
+		"Dimensione File",
+		"Terminato",
+		"L'indirizzo fornito non Ã¨ valido.",
+		"Non hai inserito un indirizzo di destinazione.",
+		"Errore di Postlet",
+		"Aggiungi",
+		"Rimuovi",
+		"Trasferisci",
+		"Help",
+		"Stato del trasferimento",
+		"Non chiudere il browser o cambiare pagina finchÃ© il trasferimento non Ã¨ finito.",
+		"Avvertimento di Postlet",
+		"File immagine",
+		"Seleziona il file da trasferire",
+		"L'indirizzo dell'help non Ã¨ valido."};
+
     /** Creates a new instance of PostletLabels */
     public PostletLabels(String l, URL codeBase) {
         System.out.println(l);
