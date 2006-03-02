5e06c1e56519061d1ba12408fa101da2b6d4ca35
Simon Rycroft <sdrycroft@users.noreply.github.com>
2006-03-02 11:40:24 +0000

    *** empty log message ***

--- Postlet/build.sh
+++ Postlet/build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-javac -g -verbose -d dist -source 1.3 -target 1.3 SRC/JAVA/*.java
+javac -g -d dist -source 1.3 -target 1.3 SRC/JAVA/*.java
 cd dist
-jar cvfm postlet.jar ../SRC/manifest *.class
+jar cvfm postlet.jar ../SRC/manifest *.class > /dev/null
 rm -rf *.class
 jarsigner postlet.jar tstkey
 rm -rf ../postlet/postlet.jar
 cp postlet.jar ../postlet/
 cd ..
-zip -r postlet.zip postlet/index.html postlet/javaUpload.php postlet/LICENCE postlet/postlet.jar postlet/README postlet/uploadTest.html
+zip -q -r postlet.zip postlet/index.html postlet/javaUpload.php postlet/LICENCE postlet/postlet.jar postlet/README postlet/uploadTest.html
+zip -q -r postlet-src.zip postlet-src/SRC/JAVA/*.java postlet-src/dist postlet-src/build.sh postlet-src/SRC/manifest postlet-src/README postlet-src/README
+sshfs sdrycroft@shell.sourceforge.net:/home/groups/p/po/postlet/htdocs ssh_sf/htdocs
+cp dist/postlet.jar ssh_sf/htdocs/example/
+fusermount -u ssh_sf/htdocs
+restartFirefox
