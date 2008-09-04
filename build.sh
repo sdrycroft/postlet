abf031396ae2563855bf254c54049fe553f83ea0
Simon Rycroft <sdrycroft@users.noreply.github.com>
2008-09-04 12:31:33 +0000

    Deleted

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ JDKBIN=/usr/java/jdk1.6.0_04/bin
 # Reminder to insert the correct version number
 # echo "Have you put the correct version number in?"
 
-# Copy source files for the source zip file
-cp SRC/JAVA/*.java postlet-src/SRC/JAVA
-
 # Compile the .java files
 $JDKBIN/javac -g -d dist -source 1.3 -target 1.3 SRC/JAVA/*.java -classpath /usr/java/jre1.6.0_06/lib/plugin.jar
 
@@ -30,4 +27,3 @@ cp postlet.jar /var/www/postlet.com/cvs
 # Finally make the release zip files
 cd ..
 zip -q -r postlet.zip postlet/index.html postlet/javaUpload.php postlet/LICENCE postlet/postlet.jar postlet/README postlet/uploadTest.html
-zip -q -r postlet-src.zip postlet-src/SRC/JAVA/*.java postlet-src/dist postlet-src/build.sh postlet-src/SRC/manifest postlet-src/README postlet-src/README
--- postlet-src/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir dist
-javac -g -verbose -d dist -source 1.3 -target 1.3 SRC/JAVA/*.java lib/*.java
-cd dist 
-jar cvfm postlet.jar ../SRC/manifest *.class netscape
-rm -rf *.class netscape
-jarsigner postlet.jar tstkey
