49bcb4192439295a116808d6b64fae342cb48dcd
Simon Rycroft <sdrycroft@users.noreply.github.com>
2008-09-04 12:27:48 +0000

    0.15 Release

--- build.sh
+++ build.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+# JDK bin directory
+JDKBIN=/usr/java/jdk1.6.0_04/bin
+
 # Reminder to insert the correct version number
-echo "Have you put the correct version number in?"
+# echo "Have you put the correct version number in?"
 
 # Copy source files for the source zip file
 cp SRC/JAVA/*.java postlet-src/SRC/JAVA
 
 # Compile the .java files
-# /usr/java/jdk1.6.0_04/bin/javac -g -d dist -source 1.3 -target 1.3 SRC/JAVA/*.java lib/netscape/javascript/*.java
-/usr/java/jdk1.6.0_04/bin/javac -g -d dist -source 1.3 -target 1.3 SRC/JAVA/*.java -classpath /usr/java/jre1.6.0_06/lib/plugin.jar
+$JDKBIN/javac -g -d dist -source 1.3 -target 1.3 SRC/JAVA/*.java -classpath /usr/java/jre1.6.0_06/lib/plugin.jar
 
 # move into the compiled folder, and package it up
 cd dist
-/usr/java/jdk1.6.0_04/bin/jar cvfm postlet.jar ../SRC/manifest *.class > /dev/null
+$JDKBIN/jar cvfm postlet.jar ../SRC/manifest *.class > /dev/null
 
 # Delete class files (paranoia)
 rm -rf *.class
 
 # Sign the jar file
-/usr/java/jdk1.6.0_04/bin/jarsigner -storepass password postlet.jar tstkey
+$JDKBIN/jarsigner -storepass password postlet.jar tstkey
 
 # Move the jar file
 rm -rf ../postlet/postlet.jar
